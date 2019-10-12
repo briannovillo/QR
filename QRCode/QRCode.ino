d02310997293d4e41bd95fe292f62c3d9275c18c
briannovillo <brian@briannovillo.com.ar>
2019-10-12 19:44:37 -0300

    Add support for scale QR

--- QRCode/QRCode.ino
+++ QRCode/QRCode.ino
@@ -1,37 +1,31 @@
-/**
- *  QRCode
- *
- *  A quick example of generating a QR code.
- *
- *  This prints the QR code to the serial monitor as solid blocks. Each module
- *  is two characters wide, since the monospace font used in the serial monitor
- *  is approximately twice as tall as wide.
- *
- */
-
 #include <qrcode.h>
 #include <UTFT.h>
 
-// Declare which fonts we will be using
-extern uint8_t BigFont[];
+#define LEFT_PADDING 40
+#define QR_VERSION 3
+#define PIXEL_SCALE 8
 
 UTFT myGLCD(ILI9341_16, 38, 39, 40, 41);
 
 void setup() {
     myGLCD.InitLCD();
-    myGLCD.setFont(BigFont);
     myGLCD.fillScr(VGA_BLACK);
     myGLCD.setColor(VGA_WHITE);
-  
+ 
     // Create the QR code
     QRCode qrcode;
-    uint8_t qrcodeData[qrcode_getBufferSize(3)];
-    qrcode_initText(&qrcode, qrcodeData, 3, 0, "ESTO ES UNA PRUEBA Y FUNCIONA WIIII");
-  
-    for (uint8_t y = 0; y < qrcode.size; y++) {
-      for (uint8_t x = 0; x < qrcode.size; x++) {
-          if (qrcode_getModule(&qrcode, x, y)) {
-              myGLCD.drawPixel(x, y);
+    uint8_t qrcodeData[qrcode_getBufferSize(QR_VERSION)];
+    qrcode_initText(&qrcode, qrcodeData, QR_VERSION, 0, "MIRAME MAMI, SIN MANOS!");
+
+    int QRSize = qrcode.size;
+    int QRScaledSize = QRSize * PIXEL_SCALE;
+    
+    for (uint8_t y = 0; y < QRScaledSize; y = y+PIXEL_SCALE) {
+      for (uint8_t x = 0; x < QRScaledSize; x = x+PIXEL_SCALE) {
+          if (qrcode_getModule(&qrcode, x/PIXEL_SCALE, y/PIXEL_SCALE)) {
+              for (uint8_t p = 0; p < PIXEL_SCALE; p++) {
+                myGLCD.drawRect(x+LEFT_PADDING, y, x+p+LEFT_PADDING, y+p);
+              }
           }
       }
     }
